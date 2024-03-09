c0eb152d8643292106cb89517b0802b09cebda92
alexsiregar <siregaralex78@gmail.com>
2024-03-09 11:38:56 +0700

    fix indentasi

--- src/Main.php
+++ src/Main.php
@@ -11,35 +11,35 @@ class Main {
   {
     $curl = curl_init();
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.fonnte.com/send',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => array(
-                'target' => $nomor_hp,
-                'message' => $message ?? 'Haloo',
-                'countryCode' => '62',
-            ),
-            CURLOPT_HTTPHEADER => array(
-                'Authorization: Y+eStfgQcsm!Dpkkiypz'
-            ),
-        )
-        );
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api.fonnte.com/send',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array(
+            'target' => $nomor_hp,
+            'message' => $message ?? 'Haloo',
+            'countryCode' => '62',
+        ),
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Y+eStfgQcsm!Dpkkiypz'
+        ),
+    )
+    );
 
-        $response = curl_exec($curl);
-        if (curl_errno($curl)) {
-            $error_msg = curl_error($curl);
-        }
-        curl_close($curl);
+    $response = curl_exec($curl);
+    if (curl_errno($curl)) {
+        $error_msg = curl_error($curl);
+    }
+    curl_close($curl);
 
-        if (isset($error_msg)) {
-            echo $error_msg;
-        }
-        echo $response;
+    if (isset($error_msg)) {
+        echo $error_msg;
+    }
+    echo $response;
   }
 }
